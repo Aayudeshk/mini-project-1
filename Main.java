75db5c5c20848a0a651a2eaec446e0f90b3d0df9
aayudeshk <aayudesh.kaparthi@patelhs.org>
2020-10-16 18:07:27 +0000

    First I declared my variable as covidCases.

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 class Main {
   public static void main(String[] args) {
-    
+    int covidCases = 3001;
+    if (covidCases < 3000);
+      System.out.print("You can go outside");
+    else 
+      System.out.print("Don't go outside");
   }
 }
