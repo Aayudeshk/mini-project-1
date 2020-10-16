8cd5dcfa577597f0b6bf3f63519f4f8d9d795158
aayudeshk <aayudesh.kaparthi@patelhs.org>
2020-10-16 18:13:55 +0000

    I then made a if else loop that tells me if i am allowed to go outside or not based on the number of Covid cases.

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
 class Main {
   public static void main(String[] args) {
     int covidCases = 3001;
-    if (covidCases < 3000);
-      System.out.print("You can go outside");
+    if (covidCases < 3000)
+     { 
+       System.out.print("You can go outside");
+     }
     else 
-      System.out.print("Don't go outside");
+    {
+      System.out.print("Don't go outside.");
+    }
   }
 }
